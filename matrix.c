eea615e5dcfd67d1edf232a058abc874c4a10963
ijh0614 <63185555+ijh0614@users.noreply.github.com>
2021-03-29 01:27:12 +0900

    make create/free matrix

--- matrix.c
+++ matrix.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /* Method Declaration */
-int** create_matrix(int row, int col);
+int** create_matrix(int row, int col);// return(반환값)이 이중포인터이다.
 void print_matrix(int** matrix, int row, int col);
 int free_matrix(int** matrix, int row, int col);
 int fill_data(int** matrix, int row, int col);
@@ -15,17 +15,17 @@ int multiply_matrix(int** matrix_a, int** matrix_t, int row, int col);
 int main()
 {
 
-    char command;
-    printf("[----- [ImJongHoon(임종훈)]  [2018038074] -----]\n");
+    char command;//사용자가 메뉴에서 입력한 값을 저장해주는 변수. 메뉴 이외의 글자 전처리검사 필요.
+    printf("[----- [임종훈]  [2018038074] -----]\n");
 
-    int row, col;
-    srand(time(NULL));
+    int row, col;//행, 열 변수 선언.
+    srand(time(NULL));// rand로 난수 생성 시 생성할 때마다 바뀐 난수를 출력하기 위해. 계속 바뀌는 시간을 넣어준다.
 
     printf("Input row and col : ");
-    scanf("%d %d", &row, &col);
-    int** matrix_a = create_matrix(row, col);
+    scanf("%d %d", &row, &col);//행과 열을 입력. 순서 주의.
+    int** matrix_a = create_matrix(row, col);//create_matrix 함수로 이동한다. 동적할당을 이용한 2차원 배열이기 때문에 이중포인터 사용.
     int** matrix_b = create_matrix(row, col);
-    int** matrix_a_t = create_matrix(col, row);
+    int** matrix_a_t = create_matrix(col, row);//전치행렬을 생성한다. 행과 열의 값을 바꾸어서 입력한다.
 
     printf("Matrix Created.\n");
 
@@ -93,9 +93,21 @@ int main()
 }
 
 /* create a 2d array whose size is row x col using malloc() */
+//행렬 생성 함수. 반환값이 이중 포인터.
 int** create_matrix(int row, int col)
-{
-
+{   
+    int**matrix;//생성해서 반환해줄 이중 포인터(이차원 배열의 주소를 저장할 변수)선언
+   
+    /*포인터 변수를 열의 크기만큼 동적할당
+   1.행의 갯수는 열의 크기만큼 존재하기 때문.
+   2. 아래의 선언한 배열에는 행의 주소들을 받아야 하기 때문에 이중포인터 선언.
+    */
+    matrix=(int**)malloc(sizeof(int*)*col);
+   
+    for(int a=0;a<col;a++){//반복문을 사용해서 행들을 동적할당해준다.
+        matrix[a]=(int*)malloc(sizeof(int) * row);//row 크기의 메모리의 주소를 전달해주는 것.
+    }
+    return matrix;
 }
 
 /* print matrix whose size is row x col */
@@ -106,9 +118,13 @@ void print_matrix(int** matrix, int row, int col)
 
 
 /* free memory allocated by create_matrix() */
+//해제 또한 반복문을 사용해서 여러번 해제해야 함을 명심!
 int free_matrix(int** matrix, int row, int col)
 {
-
+    for(int a=0;a<col;a++){
+        free(matrix[a]);//행들을 가르키고 있는 동적할당 해제
+    }
+    free(matrix);//시각적으로 보았을 때 첫번째 열인 동적할당 해제.
 }
 
 
